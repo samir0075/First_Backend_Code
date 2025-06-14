9b2c895048aa8fca57ef310636df7d49069658da
Samir <samirsingh0075@gmail.com>
2025-06-14 22:35:43 +0530

    Import the app.js file in index.js file

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,12 +1,10 @@
 import dotenv from "dotenv";
-import express from "express";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
-
-const app = express();
 
 const port = process.env.PORT || 4000;
 
