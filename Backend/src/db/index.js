254d717c57b08256bb727b2d07257e2741ac7cc5
Hts@Samir <samir@hyperthings.ai>
2025-06-25 19:08:28 +0530

    Offline Mongodb Database

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -4,7 +4,9 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URL}/${DB_NAME}`
+      `${process.env.LOCAL_MONGODB_URL}`
+      // /${DB_NAME}
+      
     );
 
     console.log(
